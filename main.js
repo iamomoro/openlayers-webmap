dc4447095573ce8d6e2425bc46ab9b5e768d6e61
iamomoro <iamomoroit@gmail.com>
2022-04-25 15:30:37 +0300

    ol/view adjustments

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ window.onload = init;
 function init() {
   const map = new ol.Map({
     view: new ol.View({
-      center: [0, 0],
-      zoom: 2,
+      center:[4101654.8454099083, -143052.03490548686],
+      zoom: 7,
+      maxZoom: 10,
+      minZoom: 7,
+      rotation: 0.5,
     }),
     layers: [
       new ol.layer.Tile({
@@ -12,5 +15,9 @@ function init() {
       }),
     ],
     target: 'js-map',
-  });
+  })
+
+  map.on('click', function(e){
+    console.log(e.coordinate);
+  })
 }
